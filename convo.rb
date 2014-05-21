91117e322589c889b552fd38cdac03d896413740
David Asabina <david@supr.nu>
2014-05-21 19:42:02 +0200

    Refactored to use blocks

--- convo.rb
+++ convo.rb
@@ -5,29 +5,26 @@ require 'rubygems'
 require 'amqp'
 
 EventMachine.run do
-  connection = AMQP.connect host: '127.0.0.1'
+  AMQP.connect host: '127.0.0.1' do |connection|
+    channel  = AMQP::Channel.new connection
 
-  channel  = AMQP::Channel.new connection
-  chinese_queue  = channel.queue 'china.convo.demo', auto_delete: true
-  russian_queue  = channel.queue 'russia.convo.demo', auto_delete: true
-  american_queue = channel.queue 'states.convo.demo', auto_delete: true
-
-  exchange = channel.direct ''
-
-  chinese_queue.subscribe do |message|
-    puts "Mr. Putin, #{message}"
+    channel.queue('china.convo', auto_delete: true).subscribe do |msg|
+      puts "Mr. Putin, #{msg}"
+    end
+  
+    channel.queue('russia.convo', auto_delete: true).subscribe do |msg|
+      puts "Mr. Jinping, #{msg}"
+    end
+  
+    channel.queue('states.convo', auto_delete: true).subscribe do |msg|
+      puts "Mr. Obama, #{msg}"
+      connection.close { EventMachine.stop }
+    end
+  
+    exchange = channel.direct ''
+  
+    exchange.publish 'dinner at your place?', routing_key: 'china.convo' 
+    exchange.publish 'subderby on friday?', routing_key: 'russia.convo'
+    exchange.publish 'is the hoop fixed? How about a 1 on 1, now?', routing_key: 'states.convo'
   end
-
-  russian_queue.subscribe do |message|
-    puts "Mr. Jinping, #{message}"
-  end
-
-  american_queue.subscribe do |message|
-    puts "Mr. Obama, #{message}"
-    connection.close { EventMachine.stop }
-  end
-
-  exchange.publish 'dinner at your place?', routing_key: chinese_queue.name
-  exchange.publish 'subderby on friday?', routing_key: russian_queue.name
-  exchange.publish 'is the hoop fixed? How about a 1 on 1, now?', routing_key: american_queue.name
 end
