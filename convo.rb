f7f397a3c4dc511853cc2a772bd4063907ee0e5a
David Asabina <david@supr.nu>
2014-05-21 19:47:50 +0200

    Refactored + fixed the addressing the wrong people in channels

--- convo.rb
+++ convo.rb
@@ -4,31 +4,29 @@
 require 'rubygems'
 require 'amqp'
 
-EventMachine.run do
-  AMQP.connect host: '127.0.0.1' do |connection|
-    channel  = AMQP::Channel.new connection
+AMQP.start host: '127.0.0.1' do |connection|
+  channel  = AMQP::Channel.new connection
 
-    channel.queue('china.convo', auto_delete: true).subscribe do |msg|
-      puts "Mr. Putin, #{msg}"
-    end
-  
-    channel.queue('russia.convo', auto_delete: true).subscribe do |msg|
-      puts "Mr. Jinping, #{msg}"
-    end
-  
-    channel.queue('states.convo', auto_delete: true).subscribe do |msg|
-      puts "Mr. Obama, #{msg}"
-    end
-  
-    exchange = channel.direct ''
-
-    EventMachine.add_timer(3) do
-      exchange.delete
-      connection.close { EventMachine.stop }
-    end
-  
-    exchange.publish 'dinner at your place?', routing_key: 'china.convo' 
-    exchange.publish 'subderby on friday?', routing_key: 'russia.convo'
-    exchange.publish 'is the hoop fixed? How about a 1 on 1, now?', routing_key: 'states.convo'
+  channel.queue('china.convo', auto_delete: true).subscribe do |msg|
+    puts "Mr. Jinping, #{msg}"
   end
+
+  channel.queue('russia.convo', auto_delete: true).subscribe do |msg|
+    puts "Mr. Putin, #{msg}"
+  end
+
+  channel.queue('states.convo', auto_delete: true).subscribe do |msg|
+    puts "Mr. Obama, #{msg}"
+  end
+
+  exchange = channel.direct ''
+
+  EventMachine.add_timer(3) do
+    exchange.delete
+    connection.close { EventMachine.stop }
+  end
+
+  exchange.publish 'dinner at your place?', routing_key: 'china.convo' 
+  exchange.publish 'submarine derby on friday?', routing_key: 'russia.convo'
+  exchange.publish 'is the hoop fixed? How about a 1 on 1, now?', routing_key: 'states.convo'
 end
